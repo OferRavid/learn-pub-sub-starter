1b9fc9621cb73d05c5be54c20ba0c0b9eb293c75
OferRavid <ofer.rvd@gmail.com>
2025-12-21 03:05:55 +0200

    Small fix in log handler

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -9,10 +9,11 @@ import (
 )
 
 func handlerLogs() func(routing.GameLog) pubsub.AckType {
-	defer fmt.Print("> ")
 	return func(gl routing.GameLog) pubsub.AckType {
+		defer fmt.Print("> ")
 		err := gamelogic.WriteLog(gl)
 		if err != nil {
+			fmt.Printf("error writing log: %v\n", err)
 			return pubsub.NackRequeue
 		}
 
